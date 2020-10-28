7d962a9f1b4515c884a0c429c7b8c4ce1b91c45c
yuktnk <yuki0wl@gmail.com>
2020-10-29 08:52:34 +0900

    chapter2-04

---
+++ footer.php
@@ -0,0 +1,28 @@
+<div class="pagetop js-pagetop"><i class="fas fa-angle-up"></i>PAGE TOP</div>
+
+<footer class="footer">
+  <div class="container">
+    <div class="footer_inner">
+      <nav>
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">わたしたちについて</a></li>
+          <li><a href="#">アクセス</a></li>
+          <li><a href="#">最新情報</a></li>
+          <li><a href="#">お問い合わせ</a></li>
+        </ul>
+      </nav>
+      <div class="footer_copyright">
+        <small>&copy; BISTRO CALME All rights reserved.</small>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<link href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/home.js"></script>
+
+</body>
+
+</html>
