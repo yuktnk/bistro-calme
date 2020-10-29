12c0fce76cbaad5aef156f13d57e016de481ead6
yuktnk <yuki0wl@gmail.com>
2020-10-29 15:33:18 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,17 +1,27 @@
 <?php
-add_theme_support('title-tag'); // <title>タグを出力する
+/**
+ * <title>タグを出力する
+ */
+add_theme_support( 'title-tag' );
 
+
+/**
+ * タイトルタグの区切り文字をエン・ダッシュから縦線に変更する
+ */
 add_filter('document_title_separator', 'my_document_title_separator');
 function my_document_title_separator($separator){
-  $separator = '|';
-  return $separator;
+    $separator = '|';
+    return $separator;
 }
 
+/**
+ * タイトルタグのテキストを変更する
+ */
 add_filter('document_title_parts', 'my_document_title_parts');
-function my_document_title_parts($title) {
-  if (is_home()) {
-    unset($title['target']); // タグラインを削除
-    $title['title'] = 'BISTRO CALMEは、カジュアルなワンバーよりなビストロです。'; // テキストを変更
-  }
-  return $title;
+function my_document_title_parts($title){
+    if (is_home()) {
+        unset($title['tagline']); // タグラインを削除
+        $title['title'] = 'BISTRO CALMEは、カジュアルなワインバーよりなビストロです。'; //テキストを変更
+    }
+    return $title;
 }
