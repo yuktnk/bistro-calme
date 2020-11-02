b0b87aa358b680382a93393fe974da1a60f72450
yuktnk <yuki0wl@gmail.com>
2020-11-02 16:53:44 +0900

    chapter3-03

--- page.php
+++ page.php
@@ -1,6 +1,10 @@
+<?php 
+	if ( is_page('contact') ) {
+		remove_filter('the_content', 'wpautop');
+	}
+?>
+
 <?php get_header(); ?>
-
-
 
 <?php if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) : the_post(); ?>
