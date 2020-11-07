801e7f65930839491fc1c39da5423a7241d7dda2
yuktnk <yuki0wl@gmail.com>
2020-11-07 16:26:35 +0900

    chapter5-02

--- template-parts/loop-menu.php
+++ template-parts/loop-menu.php
@@ -8,7 +8,7 @@
       <?php endif; ?>
     </figure>
     <h3 class="menu_title"><?php the_title(); ?></h3>
-    <p class="menu_price">800円</p>
+    <p class="menu_price"><?php the_field('price'); ?>円</p>
     <div class="menu_desc">
       <?php the_excerpt(); ?>
     </div>
