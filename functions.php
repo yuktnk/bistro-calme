369719245145df416808ef5f7e6d29d47c3a88e1
yuktnk <yuki0wl@gmail.com>
2020-11-14 12:39:50 +0900

    chapter7

--- functions.php
+++ functions.php
@@ -68,3 +68,22 @@ function my_editor_suport(){
   add_theme_support('editor-styles');
   add_editor_style('assets/css/editor-style.css');
 }
+
+// 管理者の権限グループを設定する
+add_action('admin_init', 'my_admin_init');
+function my_admin_init(){
+  // 権限を取得する（管理者＝administrator）
+  $role = get_role('administrator');
+  // 権限を追加する時
+  $role->add_cap('edit_others_menus');
+  $role->add_cap('edit_menus');
+  $role->add_cap('edit_private_menus');
+  $role->add_cap('edit_published_menus');
+  $role->add_cap('publish_menus');
+  $role->add_cap('read_private_menus');
+  // 権限を削除する時
+  $role->remove_cap('delete_others_menus');
+  $role->remove_cap('delete_menus');
+  $role->remove_cap('delete_private_menus');
+  $role->remove_cap('delete_publishes_menus');
+}
